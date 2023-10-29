7c8fd8342a94c76aa64ac6fbbacbb53a60e62808
developer-abdulali <ab.ali.soomro@gmail.com>
2023-10-29 14:08:12 +0500

    added chnages

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import CartSlice from "./slices/CartSlice"; 
+import CategorySlice from "./slices/CategorySlice";
 const Store = configureStore({
   reducer: {
     cart: CartSlice,
+    category: CategorySlice
   },
 });
 
